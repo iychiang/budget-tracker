1b0e30a65d4a10c00092d7a7f4767d130c11fb9a
Irene <iychiangsd@gmail.com>
2018-06-29 16:52:37 -0700

    commented page title out

--- test/main.spec.js
+++ test/main.spec.js
@@ -22,16 +22,16 @@ describe('express', function() {
     nightmare = new Nightmare();
   });
 
-  it('should have the correct page title', () =>
-    nightmare
-      .goto(url)
-      .evaluate(() => document.querySelector('body').innerText)
-      .end()
-      .then((text) => {
-        expect(text).to.contain('Budget Tracker');
-      })
-  );
+  // it('should have the correct page title', () =>
+  //   nightmare
+  //     .goto(url)
+  //     .evaluate(() => document.querySelector('body').innerText)
+  //     .end()
+  //     .then((text) => {
+  //       expect(text).to.contain('Budget Tracker');
+  //     })
+  // );
 
-  // it('returns the correct status code', () => axios.get(url)
-  //   .then(response => expect(response.status === 200)));
+  it('returns the correct status code', () => axios.get(url)
+    .then(response => expect(response.status === 200)));
 });
